67c1c142ec0f65576ac9e302a5e367dd918ee9f9
Jennifer Cole <capauldi@gmail.com>
2022-02-08 04:35:37 -0800

    added login/logout with session cookies

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +14,17 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+const sess = {
+    secret: process.env.SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+app.use(session(sess));
 
 // Turn on routes
 app.use(routes);
