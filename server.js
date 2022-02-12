b6f58ce90487bb378c479f126b67e33d54ec4415
Jennifer Cole <capauldi@gmail.com>
2022-02-12 04:36:05 -0800

    helpers added for date formatting, pluralizing words, and url formatting

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
 require('dotenv').config();
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,6 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Sessions
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
     secret: process.env.SECRET,
     cookie: {},
